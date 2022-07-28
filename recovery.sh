dfa502ed12dcb9a8f1be278fc6d548f811c59228
Sonsonnetwork <63415213+Sonsonnetwork@users.noreply.github.com>
2022-07-28 00:26:21 -0400

    Add files via upload

---
+++ recovery.sh
@@ -0,0 +1,32 @@
+#!/system/bin/sh
+
+list_files() {
+	echo ConfigUpdater.apk
+	#echo GoogleBackupTransport.apk
+	echo GmsCore.apk
+	echo GooglePartnerSetup.apk
+	echo GoogleLoginService.apk
+	#echo GoogleFeedback.apk
+	#echo GoogleEars.apk
+	echo OneTimeInitializer.apk
+	echo NetworkLocation.apk
+	echo MediaUploader.apk
+	#echo LatinImeDictionaryPack.apk
+	#echo GoogleTTS.apk
+	echo GoogleServicesFramework.apk
+	echo Phonesky.apk
+	echo SetupWizard.apk
+}
+
+mount -o rw,remount /system
+
+list_files | while read FILE DUMMY; do
+	rm "/system/app/$FILE"
+done
+
+pm uninstall cn.autohack.hondahack
+pm clear com.mitsubishielectric.ada.app.dalauncher
+
+rm -Rf /data/dalvik-cache/*
+
+reboot recovery
