b8a44ec1a8694d250c883e5aa87da6ababe6edf0
Sonsonnetwork <63415213+Sonsonnetwork@users.noreply.github.com>
2022-07-28 00:49:21 -0400

    Update reboot-recovery.sh

--- scripts/reboot-recovery.sh
+++ scripts/reboot-recovery.sh
@@ -1,9 +1,31 @@
 #!/system/bin/sh
-########################
-# DESCRIPTION:
-#   Reboot Android into bootloader
-#
 
-setprop ctl.start pre-recovery
-sleep 3
-reboot recovery # fallback
+list_files() {
+	echo ConfigUpdater.apk
+	#echo GoogleBackupTransport.apk
+	echo GmsCore.apk
+	echo GooglePartnerSetup.apk
+	echo GoogleLoginService.apk
+	#echo GoogleFeedback.apk
+	#echo GoogleEars.apk
+	echo OneTimeInitializer.apk
+	echo NetworkLocation.apk
+	echo MediaUploader.apk
+	#echo LatinImeDictionaryPack.apk
+	#echo GoogleTTS.apk
+	echo GoogleServicesFramework.apk
+	echo Phonesky.apk
+	echo SetupWizard.apk
+}
+
+mount -o rw,remount /system
+
+list_files | while read FILE DUMMY; do
+	rm "/system/app/$FILE"
+done
+
+pm clear com.mitsubishielectric.ada.app.dalauncher
+
+rm -Rf /data/dalvik-cache/*
+
+reboot recovery
